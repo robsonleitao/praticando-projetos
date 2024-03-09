01a6678318ce844ba53e54124595d13a9c3d5d06
Robson <robsonleitao26@gmail.com>
2024-03-09 20:35:18 -0300

    erro no formulario de contato

--- 02 - modelo de portfolio/assets/js/contact.js
+++ 02 - modelo de portfolio/assets/js/contact.js
@@ -1,1 +1,23 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.querySelector('#contact_form'),
+contactMessage = document.querySelector('#contact_message');
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendform('service_7jdpvjp', 'template_2vdxq3f', '#contact_form', 'KnuYeI7-OYNx7DYEk').then(() => {
+        // Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅';
+
+        // Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset()
+    }, () => {
+        // Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌';
+    })
+}
